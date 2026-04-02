5d6f321d2f9f9b4bbc3f59936247271f5f4af176
Jefferson Oliveira <oliveira2112@outlook.com>
2026-04-02 15:01:20 -0300

    config cors

---
+++ src/main/java/com/logap/logap_api/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.logap.logap_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+  @Value("${app.cors.origins}")
+  private String[] allowedOrigins;
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins(allowedOrigins)
+            .allowedMethods("*");
+}
+}
