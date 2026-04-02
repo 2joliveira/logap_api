c86bdd949bd0440292f52e454dab7261155cb968
Jefferson Oliveira <oliveira2112@outlook.com>
2026-04-02 07:31:11 -0300

    initial commit

---
+++ src/main/java/com/logap/logap_api/LogapApiApplication.java
@@ -0,0 +1,13 @@
+package com.logap.logap_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogapApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logap/logap_api/LogapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logap.logap_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogapApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
